47f66296617061cac50cdd585edc249bcfc928c8
Shi Wei Zheng <shiz3150@hstat.org>
2019-02-15 17:46:41 +0000

    index to result route

---
+++ application_controller.rb
@@ -0,0 +1,18 @@
+require 'dotenv/load'
+require 'bundler'
+Bundler.require
+
+require_relative 'models/model.rb'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  
+  post '/result' do
+    
+    erb :result
+  end
+  
+end
